f54e65d9a493e2cd7293841f90d5ff48529a0c80
ntalekt <ntalekt@users.noreply.github.com>
2025-02-04 13:40:09 -0700

    formatting

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,16 +1,17 @@
 import logging
 import sys
 
+
 def configure_logger(name=__name__):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    
+
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
-    
+
     ch = logging.StreamHandler(sys.stdout)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-    
+
     return logger
--- src/video_downloader.py
+++ src/video_downloader.py
@@ -1,16 +1,19 @@
 from yt_dlp import YoutubeDL
 import os
 
+
 def download_video(url):
     ydl_opts = {
-        'format': 'bestvideo[ext=mp4][vcodec!=av01]+bestaudio[ext=m4a]/best[ext=mp4]',
-        'outtmpl': os.path.join('temp', '%(id)s.%(ext)s'),
-        'postprocessors': [{
-            'key': 'ExecAfterDownload',
-            'exec_cmd': 'ffmpeg -y -i {} -c:v libx264 -preset fast {}.temp.mp4 && mv {}.temp.mp4 {}'
-        }]
+        "format": "bestvideo[ext=mp4][vcodec!=av01]+bestaudio[ext=m4a]/best[ext=mp4]",
+        "outtmpl": os.path.join("temp", "%(id)s.%(ext)s"),
+        "postprocessors": [
+            {
+                "key": "ExecAfterDownload",
+                "exec_cmd": "ffmpeg -y -i {} -c:v libx264 -preset fast {}.temp.mp4 && mv {}.temp.mp4 {}",
+            }
+        ],
     }
-    
+
     with YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         return ydl.prepare_filename(info)
